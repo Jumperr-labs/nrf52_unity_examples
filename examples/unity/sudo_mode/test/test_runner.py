f92d6ae4adce470250161e3520537e0f4eeab2a9
seryoni <seryoni@gmail.com>
2017-11-06 15:21:58 +0200

    new runner

--- examples/unity/sudo_mode/test/test_runner.py
+++ examples/unity/sudo_mode/test/test_runner.py
@@ -16,5 +16,4 @@ jemu.load(fw_bin)
 
 with jemu as j:
 	print('Virtual device is running')
-	while True:
-		j.uart.wait_until_uart_receives('----------------')
+	result = j.uart.wait_until_uart_receives('----------------')        
