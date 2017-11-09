f17b59856b3fcd2c818f769e1e27ff8b7618f61c
seryoni <seryoni@gmail.com>
2017-11-09 11:45:07 +0200

    first commit

---
+++ examples/peripherals/bme280/test/runner.py
@@ -0,0 +1,20 @@
+import unittest
+import serial
+import os
+import sys
+import subprocess
+from time import sleep
+from jemu import Jemu
+
+dir = os.path.dirname(os.path.abspath(__file__))
+fw_bin = os.path.join(dir, '..', 'pca10040', 'blank', 'armgcc', '_build', 'nrf52832_xxaa.bin')
+
+
+print(dir)
+jemu = Jemu(working_directory=dir)
+jemu.load(fw_bin)
+
+with jemu as j:
+	print('Virtual device is running')
+	while True:
+		j.uart.wait_until_uart_receives('----------------')
