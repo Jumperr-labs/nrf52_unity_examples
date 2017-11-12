db15f3610b22f0c492d807e762e7de99c9e89bd7
Dan Shemesh <dan@jumper.io>
2017-11-12 13:14:15 +0200

    config path

--- examples/unity/simple_asserts/test/test_runner.py
+++ examples/unity/simple_asserts/test/test_runner.py
@@ -3,7 +3,7 @@ from jemu import Jemu
 
 dir = os.path.dirname(os.path.abspath(__file__))
 fw_bin = os.path.join(dir, '..', 'pca10040', 'blank', 'armgcc', '_build', 'nrf52832_xxaa.bin')
-config_file = os.path.abspath(os.path.join('..', '..', '..', '..', 'jumper_config.json'))
+config_file = os.path.join(dir, '..', '..', '..', '..', 'jumper_config.json')
 
 print(dir)
 jemu = Jemu(working_directory=dir, config_file=config_file)
