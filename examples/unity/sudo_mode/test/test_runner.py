4cec9909254bbfc7c97319045b38b024516b9850
Dan Shemesh <dan@jumper.io>
2017-11-14 16:45:43 +0200

    changed name

--- examples/unity/sudo_mode/test/test_runner.py
+++ examples/unity/sudo_mode/test/test_runner.py
@@ -1,13 +1,13 @@
 import os
-from jemu import Jemu
+from jumper.vlab import Vlab
 
 dir = os.path.dirname(os.path.abspath(__file__))
 fw_bin = os.path.join(dir, '..', 'pca10040', 'blank', 'armgcc', '_build', 'nrf52832_xxaa.bin')
 
 print(dir)
-jemu = Jemu(working_directory=dir, sudo_mode=True)
-jemu.load(fw_bin)
+vlab = Vlab(working_directory=dir, sudo_mode=True)
+vlab.load(fw_bin)
 
-with jemu as j:
+with vlab as v:
     print('Virtual device is running')
-    result = j.uart.wait_until_uart_receives('----------------')
+    result = v.uart.wait_until_uart_receives('----------------')
