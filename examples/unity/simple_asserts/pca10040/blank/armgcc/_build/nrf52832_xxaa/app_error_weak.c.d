e360c8da7b91407c60e9b5f610525d389445b25b
Ron <shemeshron@yahoo.com>
2017-11-22 13:53:51 +0200

    added binaries. changed .gitignore

---
+++ examples/unity/simple_asserts/pca10040/blank/armgcc/_build/nrf52832_xxaa/app_error_weak.c.d
@@ -0,0 +1,199 @@
+_build/nrf52832_xxaa/app_error_weak.c.o: \
+ ../../../../../../components/libraries/util/app_error_weak.c \
+ ../../../../../../components/libraries/util/app_error.h \
+ /usr/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h \
+ /usr/arm-none-eabi/include/stdint.h \
+ /usr/arm-none-eabi/include/machine/_default_types.h \
+ /usr/arm-none-eabi/include/sys/features.h \
+ /usr/arm-none-eabi/include/_newlib_version.h \
+ /usr/arm-none-eabi/include/sys/_intsup.h \
+ /usr/arm-none-eabi/include/sys/_stdint.h \
+ /usr/arm-none-eabi/include/stdio.h /usr/arm-none-eabi/include/_ansi.h \
+ /usr/arm-none-eabi/include/newlib.h \
+ /usr/arm-none-eabi/include/sys/config.h \
+ /usr/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/arm-none-eabi/include/sys/cdefs.h \
+ /usr/lib/gcc/arm-none-eabi/6.3.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/6.3.1/include/stdarg.h \
+ /usr/arm-none-eabi/include/sys/reent.h \
+ /usr/arm-none-eabi/include/_ansi.h \
+ /usr/arm-none-eabi/include/sys/_types.h \
+ /usr/arm-none-eabi/include/machine/_types.h \
+ /usr/arm-none-eabi/include/sys/lock.h \
+ /usr/arm-none-eabi/include/sys/types.h \
+ /usr/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /usr/arm-none-eabi/include/machine/types.h \
+ /usr/arm-none-eabi/include/sys/stdio.h \
+ /usr/lib/gcc/arm-none-eabi/6.3.1/include/stdbool.h \
+ ../../../../../../components/device/nrf.h \
+ ../../../../../../components/device/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../components/toolchain/system_nrf52.h \
+ ../../../../../../components/device/nrf52_bitfields.h \
+ ../../../../../../components/device/nrf51_to_nrf52.h \
+ ../../../../../../components/device/nrf52_name_change.h \
+ ../../../../../../components/device/compiler_abstraction.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ ../config/sdk_config.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../components/libraries/util/app_error_weak.h \
+ ../../../../../../components/libraries/experimental_log/nrf_log.h \
+ ../../../../../../components/libraries/util/sdk_common.h \
+ /usr/arm-none-eabi/include/string.h \
+ /usr/arm-none-eabi/include/sys/string.h \
+ ../../../../../../components/device/compiler_abstraction.h \
+ ../../../../../../components/libraries/util/sdk_os.h \
+ ../../../../../../components/libraries/util/app_util.h \
+ ../../../../../../components/libraries/util/sdk_macros.h \
+ ../../../../../../components/libraries/experimental_section_vars/nrf_section.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../components/libraries/experimental_log/src/nrf_log_internal.h \
+ ../../../../../../components/libraries/util/app_util.h \
+ ../../../../../../components/libraries/experimental_log/nrf_log_ctrl.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/libraries/experimental_log/src/nrf_log_ctrl_internal.h \
+ ../../../../../../components/libraries/experimental_log/nrf_log_backend_interface.h \
+ ../../../../../../components/libraries/experimental_memobj/nrf_memobj.h \
+ /usr/arm-none-eabi/include/stdlib.h \
+ /usr/arm-none-eabi/include/machine/stdlib.h \
+ ../../../../../../components/libraries/balloc/nrf_balloc.h \
+ ../../../../../../components/libraries/util/app_util_platform.h \
+ ../../../../../../components/libraries/util/nrf_assert.h \
+ ../../../../../../components/libraries/strerror/nrf_strerror.h
+
+../../../../../../components/libraries/util/app_error.h:
+
+/usr/lib/gcc/arm-none-eabi/6.3.1/include/stdint.h:
+
+/usr/arm-none-eabi/include/stdint.h:
+
+/usr/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/arm-none-eabi/include/sys/features.h:
+
+/usr/arm-none-eabi/include/_newlib_version.h:
+
+/usr/arm-none-eabi/include/sys/_intsup.h:
+
+/usr/arm-none-eabi/include/sys/_stdint.h:
+
+/usr/arm-none-eabi/include/stdio.h:
+
+/usr/arm-none-eabi/include/_ansi.h:
+
+/usr/arm-none-eabi/include/newlib.h:
+
+/usr/arm-none-eabi/include/sys/config.h:
+
+/usr/arm-none-eabi/include/machine/ieeefp.h:
+
+/usr/arm-none-eabi/include/sys/cdefs.h:
+
+/usr/lib/gcc/arm-none-eabi/6.3.1/include/stddef.h:
+
+/usr/lib/gcc/arm-none-eabi/6.3.1/include/stdarg.h:
+
+/usr/arm-none-eabi/include/sys/reent.h:
+
+/usr/arm-none-eabi/include/_ansi.h:
+
+/usr/arm-none-eabi/include/sys/_types.h:
+
+/usr/arm-none-eabi/include/machine/_types.h:
+
+/usr/arm-none-eabi/include/sys/lock.h:
+
+/usr/arm-none-eabi/include/sys/types.h:
+
+/usr/arm-none-eabi/include/sys/_pthreadtypes.h:
+
+/usr/arm-none-eabi/include/machine/types.h:
+
+/usr/arm-none-eabi/include/sys/stdio.h:
+
+/usr/lib/gcc/arm-none-eabi/6.3.1/include/stdbool.h:
+
+../../../../../../components/device/nrf.h:
+
+../../../../../../components/device/nrf52.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmSimd.h:
+
+../../../../../../components/toolchain/system_nrf52.h:
+
+../../../../../../components/device/nrf52_bitfields.h:
+
+../../../../../../components/device/nrf51_to_nrf52.h:
+
+../../../../../../components/device/nrf52_name_change.h:
+
+../../../../../../components/device/compiler_abstraction.h:
+
+../../../../../../components/libraries/util/sdk_errors.h:
+
+../../../../../../components/drivers_nrf/nrf_soc_nosd/nrf_error.h:
+
+../config/sdk_config.h:
+
+../../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../../components/libraries/util/app_error_weak.h:
+
+../../../../../../components/libraries/experimental_log/nrf_log.h:
+
+../../../../../../components/libraries/util/sdk_common.h:
+
+/usr/arm-none-eabi/include/string.h:
+
+/usr/arm-none-eabi/include/sys/string.h:
+
+../../../../../../components/device/compiler_abstraction.h:
+
+../../../../../../components/libraries/util/sdk_os.h:
+
+../../../../../../components/libraries/util/app_util.h:
+
+../../../../../../components/libraries/util/sdk_macros.h:
+
+../../../../../../components/libraries/experimental_section_vars/nrf_section.h:
+
+../../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../../components/libraries/experimental_log/src/nrf_log_internal.h:
+
+../../../../../../components/libraries/util/app_util.h:
+
+../../../../../../components/libraries/experimental_log/nrf_log_ctrl.h:
+
+../../../../../../components/libraries/util/sdk_errors.h:
+
+../../../../../../components/libraries/experimental_log/src/nrf_log_ctrl_internal.h:
+
+../../../../../../components/libraries/experimental_log/nrf_log_backend_interface.h:
+
+../../../../../../components/libraries/experimental_memobj/nrf_memobj.h:
+
+/usr/arm-none-eabi/include/stdlib.h:
+
+/usr/arm-none-eabi/include/machine/stdlib.h:
+
+../../../../../../components/libraries/balloc/nrf_balloc.h:
+
+../../../../../../components/libraries/util/app_util_platform.h:
+
+../../../../../../components/libraries/util/nrf_assert.h:
+
+../../../../../../components/libraries/strerror/nrf_strerror.h:
